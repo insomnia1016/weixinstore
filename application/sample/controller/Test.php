61c9aa106ddb16829664fcad3d68807aa089c3da
insomnia1016 <gaoyuejianwu@qq.com>
2018-07-10 23:39:53 +0800

    微信小应用商店

---
+++ application/sample/controller/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ch
+ * Date: 2018/7/10
+ * Time: 22:19
+ */
+
+namespace app\sample\controller;
+
+
+class Test
+{
+    function Hello(){
+        return 'Hello, 江中白条';
+    }
+}
